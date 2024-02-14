5fcadb34a2c75d48196e485e5a1a8a3222b43c89
M Shahid <mdshahidraja0909@gmail.com>
2024-02-14 18:33:44 +0530

    Update SecoundCalculator.java

--- SecoundCalculator.java
+++ SecoundCalculator.java
@@ -4,7 +4,7 @@ public class SecoundCalculator {
 
 	public static void main(String[] args) 
 	{
-			System.out.println("         SMART CALCULATOR 1 45 hgy  ght vbfg ghft");
+			System.out.println("         SMART CALCULATOR 1 45 hgy  ght vbfg ghft nmhy");
 			System.out.println("...........................................");
 			System.out.println();
 			System.out.println("1. ADDITION.");
