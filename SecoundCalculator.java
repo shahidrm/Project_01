a5e8ab7fa39ec9ea1696d2e5b4cf2ac61ddc2101
M Shahid <mdshahidraja0909@gmail.com>
2024-02-14 21:15:08 +0530

    Update SecoundCalculator.java

--- SecoundCalculator.java
+++ SecoundCalculator.java
@@ -1,7 +1,7 @@
 package com.secound.calculator.project;
 import java.util.Scanner;
 public class SecoundCalculator {
-
+//abc
 	public static void main(String[] args) 
 	{
 			System.out.println("         SMART CALCULATOR 1 45 hgy  ght vbfg ghft nmhy");
